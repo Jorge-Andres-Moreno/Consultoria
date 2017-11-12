eed12adc387d31a4a3aeb4fef7047588848c993c
Jorge Andres Moreno <jorgemoreno97@hotmail.com>
2017-11-11 22:24:44 -0500

    Modified class

---
+++ Consulta_APP/app/src/main/java/com/jorgemoreno971/consulta_app/Control/Executable.java
@@ -0,0 +1,12 @@
+package com.jorgemoreno971.consulta_app.Control;
+
+/**
+ * Created by jormo on 11/11/2017.
+ */
+
+public class Executable {
+
+    public static void main(String args[]) {
+
+    }
+}
